df98b59c029a49c445879ed16ddff274a2d426b5
吉田豊 <>
2020-03-08 22:04:23 +0900

    delete unused file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 //const env = require("./key.js");
 const key = SPREAD_SHEET_KEY
-
-function myFunction() {
-
-      var spreadsheet = SpreadsheetApp.openById(key);
-      var sheet = spreadsheet.getActiveSheet();
-      
-      for (var i=1; i<10; i++){
-        var range = sheet.getRange("A"+i);
-        var value = range.getValue();
-        Logger.log(value);
-        if(value == false){
-            sheet.getRange("A"+i).setValue('insert_test')
-            break;
-          }
-      }
-    }
     
 function doGet(e) {
   let data = {};  
@@ -30,7 +14,7 @@ function doGet(e) {
   out =  ContentService.createTextOutput()
   out.setMimeType(ContentService.MimeType.JSON);
   out.setContent(data)
-  
+
   return out
 }
 
@@ -41,17 +25,11 @@ function doPost(e) {
   console.log(params);
   console.log(params.text)
   var tet = String(params);
-  console.log(typeof tet);
   console.log(tet);
   var spreadsheet = SpreadsheetApp.openById(key);
   var sheet = spreadsheet.getActiveSheet();
   var date = new Date();
-  
-  console.log(JSON.stringify(date))
-    
   var test = sheet.appendRow([date, tet]);
-  //date = String(date)
-  //data = date.getFullYear()+":" +(date.getMonth()+1)
   var output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
   output.setContent(JSON.stringify({ success: true,
@@ -62,7 +40,6 @@ function doPost(e) {
   //output.setContent(JSON.stringify({ w: tet}));
   return output;
 }
-
 
 //chromeからきたgetをうけ実行、ユニークな配列を返す
 //http通信部分を作る
@@ -81,30 +58,16 @@ function uniq(array) {
   return Array.from(knownElements);
 }
 
-function getDate(){
-  var time = new Date();
-  var hour = time.getHours();
-  var minute = time.getMinutes();
-  var second = time.getSeconds();
-  var temp = '' + ((hour > 12) ? hour - 12 : hour);
-  if (hour == 0)
-    temp = '12';
-  temp += ((minute < 10) ? ':0' : ':') + minute;
-  temp += ((second < 10) ? ':0' : ':') + second;
-  temp += (hour >= 12) ? ' P.M.' : ' A.M.';
-  return temp;
-};
-
 function doPostTest() {
   //eの作成
  //var e = ContentService.createTextOutput();
- var obj = {
+ let obj = {
     postData: {getDataAsString: word},
     age: 30,
     area: 'Tokyo'
 }
  //var e = {"name":"太郎","age":30,"area":"Tokyo"};
- var e = JSON.stringify( obj );
+ let e = JSON.stringify( obj );
  //e.setMimeType(ContentService.MimeType.JSON);
  //e.setContent(JSON.stringify({ text: true}));
  
